69870ec646a0127ebd5f73046f54ec94402ef374
PROMETHIA <electriccobras@gmail.com>
2024-02-06 17:52:18 -0500

    add UnboundVariableException

--- Functions/Variable.cs
+++ Functions/Variable.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Functions;
 
 /// <summary>
@@ -5,5 +7,27 @@ namespace Functions;
 /// </summary>
 public record Variable(string Name) : IFunctionAST
 {
-    public double Evaluate(EvalContext ctx) => ctx.Variables[Name];
+    public double Evaluate(EvalContext ctx)
+    {
+        if (ctx.Variables.TryGetValue(Name, out double value))
+            return value;
+        else
+            throw new UnboundVariableException() { Variable = Name };
+    }
+}
+
+/// <summary>
+/// Thrown when a variable in a function AST is evaluated without that variable being present in the 
+/// evaluation context. Make sure to add all relevant variables to the evaluation context before
+/// calling `IFunctionAST.Evaluate`.
+/// </summary>
+public class UnboundVariableException : Exception
+{
+    public string Variable { get; init; }
+
+    public UnboundVariableException() : base() { }
+
+    public UnboundVariableException(string message) : base(message) { }
+
+    public UnboundVariableException(string message, Exception inner) : base(message, inner) { }
 }
