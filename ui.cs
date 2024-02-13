4e9335671d6e6fcc18772cddb190b658e656f1b7
spikechris <christian.d.manuel@gmail.com>
2024-02-13 16:04:20 -0500

    Started UI
    Rough sketch from Leslie's drawing

---
+++ ui.cs
@@ -0,0 +1,16 @@
+// This is the C# shcript, I forget which one we're using
+using Godot;
+using System;
+
+public partial class ui : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ ui.gd
@@ -0,0 +1,20 @@
+# This is the C# shcript, I forget which one we're using
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_add_function__button_pressed():
+	print("hi")
