b3e1dbbf526070e230540e0001375719176a685b
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2024-02-17 12:40:54 -0500

    Fixed dotnet issue caused by merging of previous project

--- ui.cs
+++ ui.cs
@@ -4,13 +4,8 @@ using System;
 
 public partial class ui : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	private void OnButtonPressed()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		GD.Print("TEST");
 	}
 }
