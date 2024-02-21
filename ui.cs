0b8b14f6dbb8ff0e28d59eb1d57d48983783b34b
Mark Williams <mark.williams1703@gmail.com>
2024-02-21 12:07:51 -0500

    Removed ui.cs and fixed up ui.gd

--- ui.cs
+++
@@ -1,7 +0,0 @@
-// This is the C# shcript, I forget which one we're using
-using Godot;
-using System;
-
-public partial class ui : Node2D
-{
-}
--- ui.gd
+++ ui.gd
@@ -1,20 +1,9 @@
-# This is the C# shcript, I forget which one we're using
+# Base line GD UI script
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	
-
-
 func _on_exit_button_pressed():
-	get_tree().quit()
+    get_tree().quit()
 
 
 func _on_add_function__button_pressed():
-	print("hi")
+    print("hi")
