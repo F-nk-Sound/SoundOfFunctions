5e089068aa72456396ffc4612c823452e6ea5e81
PROMETHIA <electriccobras@gmail.com>
2024-02-24 10:12:56 -0500

    undo changes to PlayFunction

--- PlayFunctions.cs
+++ PlayFunctions.cs
@@ -1,29 +1,30 @@
 using Godot;
+using System;
 
 public partial class PlayFunctions : Node
 {
-	[Export] public AudioStreamPlayer? Player;
+	[Export] public AudioStreamPlayer Player;
 
-	private AudioStreamGeneratorPlayback? playback;
+	private AudioStreamGeneratorPlayback playback;
 	private float sampleRate;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		AudioStreamGenerator gen = (AudioStreamGenerator)Player!.Stream;
+		AudioStreamGenerator gen = (AudioStreamGenerator)Player.Stream;
 		sampleRate = gen.MixRate;
-		// Player.Play();
+		Player.Play();
 		playback = (AudioStreamGeneratorPlayback)Player.GetStreamPlayback();
 		FillBuffer();
 	}
 
-	void FillBuffer() 
+	void FillBuffer()
 	{
-		int frames = playback!.GetFramesAvailable();
+		int frames = playback.GetFramesAvailable();
 		double phase = 0;
 		float increment = 440 / sampleRate;
 
-		for (int i = 0; i < frames; i++) 
+		for (int i = 0; i < frames; i++)
 		{
 			playback.PushFrame(Vector2.One * (float)(4 * Mathf.Floor(phase) - 2 * Mathf.Floor(2 * phase) + 1));
 			phase += increment;
