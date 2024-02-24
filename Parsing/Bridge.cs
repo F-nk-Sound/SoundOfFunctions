15e16413580d426f772733bca3836e716e204dfd
PROMETHIA <electriccobras@gmail.com>
2024-02-24 09:57:31 -0500

    Update to handle new parser

--- Parsing/Bridge.cs
+++ Parsing/Bridge.cs
@@ -12,11 +12,88 @@ namespace Parsing;
 public unsafe struct CtorTable
 {
     // Fields go below here
+    // ### ALWAYS ENSURE FIELDS ARE IN THE CORRECT ORDER ON BOTH SIDES ###
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newAbsolute = &NewAbsolute;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr, IntPtr> newAdd = &NewAdd;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newCeil = &NewCeil;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newCosine = &NewCosine;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr, IntPtr> newDivide = &NewDivide;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr, IntPtr> newExponent = &NewExponent;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newFloor = &NewFloor;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr, IntPtr> newMultiply = &NewMultiply;
     delegate* unmanaged[Cdecl]<double, IntPtr> newNumber = &NewNumber;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newSine = &NewSine;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr, IntPtr> newSubtract = &NewSubtract;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newTangent = &NewTangent;
+    delegate* unmanaged[Cdecl]<IntPtr, IntPtr> newVariable = &NewVariable;
+    delegate* unmanaged[Cdecl]<byte*, nuint, IntPtr> newString = &NewString;
 
     // Non-fields go below here
     [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
     unsafe static IntPtr NewNumber(double value) => NewHandle(new Number(value));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewString(byte* ptr, nuint len)
+    {
+        string str = Encoding.UTF8.GetString(ptr, (int)len);
+        return NewHandle(str);
+    }
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewVariable(IntPtr name) => NewHandle(new Variable(StringFromHandle(name)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewAdd(IntPtr l, IntPtr r) => NewHandle(
+        new Add(
+            FromHandle(l),
+            FromHandle(r)
+        )
+    );
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewSubtract(IntPtr l, IntPtr r) => NewHandle(
+       new Subtract(
+           FromHandle(l),
+           FromHandle(r)
+       )
+   );
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewMultiply(IntPtr l, IntPtr r) => NewHandle(
+       new Multiply(
+           FromHandle(l),
+           FromHandle(r)
+       )
+   );
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewDivide(IntPtr l, IntPtr r) => NewHandle(
+         new Divide(
+             FromHandle(l),
+             FromHandle(r)
+         )
+     );
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewExponent(IntPtr b, IntPtr p) => NewHandle(new Exponent(FromHandle(b), FromHandle(p)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewAbsolute(IntPtr inner) => NewHandle(new Absolute(FromHandle(inner)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewCeil(IntPtr inner) => NewHandle(new Ceil(FromHandle(inner)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewFloor(IntPtr inner) => NewHandle(new Floor(FromHandle(inner)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewSine(IntPtr inner) => NewHandle(new Sine(FromHandle(inner)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewCosine(IntPtr inner) => NewHandle(new Cosine(FromHandle(inner)));
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    unsafe static IntPtr NewTangent(IntPtr inner) => NewHandle(new Tangent(FromHandle(inner)));
 
     /// <summary>
     /// A list of all GCHandles currently allocated, to be cleared once the foreign code is done 
@@ -32,6 +109,10 @@ public unsafe struct CtorTable
         activeHandles.Value!.Add(handle);
         return GCHandle.ToIntPtr(handle);
     }
+
+    static IFunctionAST FromHandle(IntPtr handle) => (IFunctionAST)GCHandle.FromIntPtr(handle).Target!;
+
+    static string StringFromHandle(IntPtr handle) => (string)GCHandle.FromIntPtr(handle).Target!;
 
     /// <summary>
     /// Free all currently active handles to prevent a memory leak. May result in undesirable behavior if
@@ -53,7 +134,7 @@ public static class Bridge
     [DllImport(dllName: "fnky_parser", CallingConvention = CallingConvention.Cdecl, EntryPoint = "fnky_parse")]
     private unsafe static extern IntPtr Parse(byte* input, nuint length, CtorTable* table);
 
-    public static IFunctionAST Parse(string input)
+    public static object Parse(string input)
     {
         CtorTable table = new();
 
@@ -70,7 +151,7 @@ public static class Bridge
                 CtorTable.FreeHandles();
                 if (target is null)
                     throw new NullReferenceException("Parsing module returned a null reference.");
-                return (IFunctionAST)target;
+                return target;
             }
         }
     }
--- Parsing/Tests.cs
+++ Parsing/Tests.cs
@@ -12,8 +12,34 @@ public class Tests
     [InlineData("-99999999.99", -99999999.99)]
     public void NumbersParse(string input, double value)
     {
-        IFunctionAST ast = Bridge.Parse(input);
-        Assert.True(ast is Number);
-        Assert.Equal(value, ((Number)ast).Value);
+        object ast = Bridge.Parse(input);
+        if (ast is Number num)
+            Assert.Equal(value, num.Value);
+        else
+            Assert.Fail((string)ast);
+    }
+
+    [Theory]
+    [InlineData("x")]
+    [InlineData("x_1")]
+    [InlineData("α_21x")]
+    [InlineData("aaΑ_a32f")]
+    public void VariablesParse(string input)
+    {
+        object ast = Bridge.Parse(input);
+        if (ast is Variable var)
+            Assert.Equal(input, var.Name);
+        else
+            Assert.Fail((string)ast);
+    }
+
+    [Theory]
+    [InlineData("3 + 3", 6)]
+    public void ArithmeticCalculations(string input, double output) {
+        object ast = Bridge.Parse(input);
+        if (ast is IFunctionAST func)
+            Assert.Equal(output, func.EvaluateAtT(0));
+        else
+            Assert.Fail((string)ast);
     }
 }
