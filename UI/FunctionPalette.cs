35141807bd20262c513a9c490ecb83243d18f781
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2024-02-25 22:08:05 -0500

    Test run, added some features to be discussed in later commit

--- UI/FunctionPalette.cs
+++ UI/FunctionPalette.cs
@@ -1,27 +1,34 @@
+using Functions;
 using Godot;
 using System;
 
 public partial class FunctionPalette : Node
 {
-	private VBoxContainer _container;
-	private Resource _textUpdateScript;
+	public IFunctionAST CurrentSelectedFunction { get; private set; }
+
+	private VBoxContainer? _container;
+	private Resource? _textUpdateScript;
+	private PackedScene? _functionContainer;
 	public override void _Ready()
 	{
 		base._Ready();
 
 		ScrollContainer scrollContainer = GetNode<ScrollContainer>("ScrollContainer");
-		_container = scrollContainer.GetNode<VBoxContainer>("FunctionContainer");
+		_container = scrollContainer.GetNode<VBoxContainer>("FunctionsContainer");
 
 		Resource _textUpdateScript = GD.Load("res://UI/TextUpdate.cs");
+		_functionContainer = GD.Load<PackedScene>("res://UI/FunctionContainer.tscn");
+
 	}
 
 	private void OnButtonPressed()
 	{
-		Container sizeContainer = new Container();
+		if (_container == null || _functionContainer == null) return;
+		Control sizeContainer = new Control();
 		sizeContainer.CustomMinimumSize = new Vector2(225, 70);
-        var FunctionContainer = GD.Load<PackedScene>("res://UI/FunctionContainer.tscn");
-		var instance = FunctionContainer.Instantiate();
-		sizeContainer.AddChild(instance);
-		_container.AddChild(sizeContainer);
+		Control instance = (Control) _functionContainer.Instantiate();
+		instance.CustomMinimumSize = new Vector2(225, 70);
+		//sizeContainer.AddChild(instance);
+		_container.AddChild(instance);
 	}
 }
--- UI/TextUpdate.cs
+++ UI/TextUpdate.cs
@@ -1,26 +1,30 @@
+using CSharpMath.Structures;
+using Functions;
 using Godot;
+using Parsing;
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public partial class TextUpdate : TextEdit
+public partial class TextUpdate : Control
 {
-	LaTeX latex;
+	ScrollContainer? lsc;
+	LaTeX? latex;
+	LineEdit? text;
 	double timeBetween = 0.75;
 	double total = 0.0;
 	bool active = true;
 
 	private void make()
 	{
+		if (latex == null || text == null) return;
 		latex.Render();
 		Vector2 size;
-		//Math.Max(this.Position.Y / 2 + latex.Height / 2, 30);
 		size = new Vector2(Math.Max(this.Position.X / 2 + latex.Width / 2, 225), Math.Max(this.Position.Y / 2 + latex.Height / 2, 60));
-		this.Size = size;
+		text.Size = size;
 		latex.Position = new Vector2(latex.Width / 2, size.Y / 2);
-		// this.Position.X / 2 + size.X / 2
-		latex.Render();
-		
+		GD.Print(latex.Position);
+		latex.Render();	
 	}
 	
 	// Called when the node enters the scene tree for the first time.
@@ -28,16 +32,15 @@ public partial class TextUpdate : TextEdit
 	{
 		base._Ready();
 
-		Node node;
-		node = this.GetParent<Node>();
-		foreach(Node child in node.GetChildren())
-		{
-			if (child.Name == "LaTeX")
-			{
-				latex = (LaTeX) child;
-				break;
-			}
-		}
+		lsc = this.GetChild<ScrollContainer>(0);
+		lsc.CustomMinimumSize = new Vector2(225, 70);
+		latex = lsc.GetChild<LaTeX>(0);
+		text = this.GetChild<LineEdit>(1);
+
+		if (latex == null)
+			GD.PrintErr("Latex not initialized");
+		if (text == null)
+			GD.PrintErr("Text not initialized");
 
 		this.OnTextChanged();
 	}
@@ -54,12 +57,52 @@ public partial class TextUpdate : TextEdit
 	
 	private void OnTextChanged()
 	{
-		String newText = this.Text;
+		if (latex == null || text == null) return;
+		String newText = text.Text;
 		if (newText.Equals(""))
-			latex.LatexExpression = this.PlaceholderText;
+			latex.LatexExpression = text.PlaceholderText;
 		else
 			latex.LatexExpression = newText;
-
-		this.make();
 	}
+
+	private void OnTextInput(InputEvent @event)
+	{
+		if (latex == null || text == null) return;
+		if (!(@event is InputEventKey keyEvent && keyEvent.Pressed)) return;
+		if (keyEvent.Keycode != Key.Enter) return;
+	}
+
+	private void OnTextSet()
+	{
+		if (latex == null || text == null) return;
+		GD.Print(text.Text);
+	}
+
+	private void _LineEditSubmitted(String finalText)
+	{
+		if (latex == null || text == null) return;
+		GD.Print(finalText);
+		make();
+		text.ReleaseFocus();
+		
+		return;
+		String functionText = text.Text;
+		ParseResult result = Bridge.Parse(functionText);
+		IFunctionAST ast = result.Unwrap();
+		for (int i = 0; i < 100; i++)
+			GD.PrintRaw(ast.EvaluateAtT(i).ToString() + " ");
+	}
+	
+	private void _OnFocusEntered()
+	{
+		text.RemoveThemeColorOverride("font_color");
+		latex.ZIndex = 1;
+	}
+
+	private void _OnFocusExited()
+	{
+		text.AddThemeColorOverride("font_color", new Color(0.0f, 0.0f, 0.0f, 0.0f));
+		latex.ZIndex = 0;
+	}
+
 }
